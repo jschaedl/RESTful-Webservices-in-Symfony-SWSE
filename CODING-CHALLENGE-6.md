2ad882c6ce64d5059b977e083cc6143454fa3495
Jan Schädlich <schaedlich.jan@gmail.com>
2021-04-10 19:15:39 +0100

    coding challenge 6 solution

--- CODING-CHALLENGE-6.md
+++ CODING-CHALLENGE-6.md
@@ -9,4 +9,7 @@
 
 ### Solution
 
-...
+- use an `ArgumentValueResolver` to deserialize the request's content
+- use the EntityManager to save the object into the database
+- for CREATE: use method `POST`, return HTTP 201 (Created) Status Code and set the Location header with the help of the `UrlGeneratorInterface`
+- for UPDATE: use method `PUT`, return HTTP 204 (No Content) Status Code and leave the response body empty
---
+++ tests/Controller/Attendee/CreateControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller\Attendee;
+
+use App\Entity\Attendee;
+use App\Repository\AttendeeRepository;
+use App\Tests\ApiTestCase;
+
+class CreateControllerTest extends ApiTestCase
+{
+    public function test_it_should_create_an_attendee(): void
+    {
+        $this->browser->request('POST', '/attendees', [], [], [],
+            <<<'EOT'
+{
+    "firstname": "Paul",
+	"lastname": "Paulsen",
+	"email": "paul@paulsen.de"
+}
+EOT
+        );
+
+        static::assertResponseStatusCodeSame(201);
+        static::assertResponseHasHeader('Location');
+
+        $attendees = static::$container->get(AttendeeRepository::class)->findAll();
+        static:: assertCount(1, $attendees);
+
+        /** @var Attendee $expectedAttendee */
+        $expectedAttendee = $attendees[0];
+        static::assertSame('Paul', $expectedAttendee->getFirstname());
+        static::assertSame('Paulsen', $expectedAttendee->getLastname());
+        static::assertSame('paul@paulsen.de', $expectedAttendee->getEmail());
+    }
+}
